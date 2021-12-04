a52174c7efde850b872e09226c2e7698b006757c
higakijin <hm385.chejptks@gmail.com>
2021-12-04 22:16:37 +0900

    詳細ページののレイアウトの調整

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -3,13 +3,13 @@
     <Navbar />
     <div class="grid grid-cols-7 gap-4">
       <div class="col-span-7 xl:col-span-1 lg:col-span-1"></div>
-      <div class="col-span-7 xl:col-span-4 lg:col-span-4 pt-36">
-        <h1 class="text-4xl font-sans">ここにタイトル</h1>
-        <p>名無し侍</p>
-        <p>2001.9.11</p>
-        
+      <div class="col-span-7 xl:col-span-4 lg:col-span-4 pt-36 px-5 pb-12">
+        <h1 class="text-4xl font-sans mb-3">ここにタイトル</h1>
         <rateBar :agree_rate="agree_rate" :disagree_rate="disagree_rate"/>
-        
+        <div class="flex items-center mt-2">
+          <p class="font-semibold title-font text-gray-700mr-3">by 名無し侍</p>
+          <p class="mt-1 text-gray-500 text-sm ml-auto">2001.09.11</p>
+        </div>
       </div>
       <div class="col-span-7 xl:col-span-2 lg:col-span-2">
         <createForm />
