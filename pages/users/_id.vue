bae488fdd0d4ea49e62d76a758ed16a3b85d3b4a
higakijin <hm385.chejptks@gmail.com>
2021-12-16 10:27:16 +0900

    投稿一覧の共通コンポーネント化とユーザー詳細ページに投稿リストを表示

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -4,23 +4,15 @@
     <div class="grid grid-cols-7 gap-4">
       <div class="col-span-7 xl:col-span-1 lg:col-span-1"></div>
       <div class="col-span-7 xl:col-span-4 lg:col-span-4 pt-32 mx-5 pb-12">
-        <h1><i class="font-bold text-4xl">{{ posts[0].name }}</i><span class="text-xl ml-2">の投稿一覧</span></h1>
+        <h1><i class="font-bold text-4xl">{{ user.name }}</i><span class="text-xl ml-2">の投稿一覧</span></h1>
         <section class="text-gray-600 body-font overflow-hidden">
-          <div class="container px-5 pt-32 pb-12 mx-auto">
-            <div class="-my-8 divide-y-2 divide-gray-100">
-              <div v-for='post in posts' :key='post.id'>
-                {{ post.topic }}
-              </div>
-            </div>
-          </div>
+          <Lists :posts="posts" @getPosts="getPosts()" />
         </section>
+
       </div>
       <div class="col-span-7 xl:col-span-2 lg:col-span-2">
         <createForm />
       </div>
-      <!-- <div class="col-span-7 xl:col-span-2 lg:col-span-2">
-        <confirmModal :post="post" ref="child" @vote="vote" />
-      </div> -->
     </div>
     
   </div>
@@ -28,25 +20,41 @@
 
 <script>
 import Navbar from '../../components/Navbar.vue'
-import rateBar from '../../components/rateBar.vue'
 import createForm from '../../components/createForm.vue'
-import confirmModal from '../../components/confirmModal.vue'
+import Lists from '../../components/posts/Lists.vue'
 
 export default {
-  components: { Navbar, createForm, confirmModal, rateBar },
+  components: { Navbar, createForm, Lists },
 
   async asyncData (context) {
     try {
-      const res = await context.$axios.$get(`/api/posts`)
+      const res = await context.$axios.$get(`/api/users/${Number(context.params.id)}`)
       return {
-        posts: res
-              .filter((v) => v.is_published && v.user_id === Number(context.params.id))
-              .sort((a, b) => { return (a.created_at < b.created_at ? 1 : -1) })
+        user: res,
+        posts: res.posts
+                  .filter((v) => v.is_published)
+                  .sort((a, b) => { return (a.created_at < b.created_at ? 1 : -1) })
       }
-    } catch (error) {
+    } catch(error) {
       console.log(error)
     }
   },
+
+  methods: {
+    async getPosts() {
+      try {
+        const res = await this.$axios.$get(`/api/users/${Number(this.$route.params.id)}`)
+        if (!res) {
+          new Error('メッセージを取得できませんでした。')
+        }
+        this.posts = res.posts
+                    .filter((v) => v.is_published)
+                    .sort(function(a, b){ return (a.created_at < b.created_at ? 1 : -1) })
+      } catch(error) {
+        console.log(error);
+      }
+    }
+  }
 
 }
 </script>
