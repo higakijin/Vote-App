af4266f0f53db57610b09b06aaba4fa743b23cca
higakijin <hm385.chejptks@gmail.com>
2021-12-26 11:04:50 +0900

    ユーザー詳細画面の再編集

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,39 +1,45 @@
 <template>
   <div class="relative">
     <Navbar />
-    <div class="grid grid-cols-7 gap-4">
-      <div class="col-span-7 xl:col-span-1 lg:col-span-1"></div>
-      <div class="col-span-7 xl:col-span-4 lg:col-span-4 pt-28 mx-5 pb-12">
-        <div>
-          <div v-if="error" class="text-red-500">
-            {{ error }}
-          </div>
-          <div v-else class="mt-8 mb-2">
-            <h1>
-              <i class="font-bold text-4xl">{{ user.name }}</i><span class="text-xl ml-2">の投稿一覧</span>
-            </h1>
-          </div>
-          <div v-show="isMyUser()" class="flex">
-            <nuxt-link :to='`${user.id}/edit`' class="ml-auto px-3 py-1 text-green-500 border border-green-500 my-auto rounded-md hover:bg-green-500 hover:text-white">
-              ユーザー設定
-            </nuxt-link>
-          </div>
 
+    <section class="text-gray-600 body-font">
+      <div class="container lg:px-5 xl:px-5 py-24 mx-auto flex flex-col">
+        <div class="lg:w-5/6 lg:mx-auto xl:mx-auto mx-1">
+
+          <div class="flex flex-col sm:flex-row mt-10">
+            <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+              <div class="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                  <circle cx="12" cy="7" r="4"></circle>
+                </svg>
+              </div>
+              <div class="flex flex-col">
+                <h2 class="font-medium title-font mt-4 text-gray-900 text-lg text-center">{{ user.name }}</h2>
+                <p class="text-sm text-gray-400 text-right mb-2 mr-4">{{ user.created_at | moment2 }}~</p>
+                <div v-show="isMyUser()" class="flex mb-4">
+                  <nuxt-link :to='`${user.id}/edit`' class="ml-auto px-3 py-1 text-green-500 border border-green-500 my-auto rounded-md hover:bg-green-500 hover:text-white">
+                    ユーザー設定
+                  </nuxt-link>
+                </div>
+                <p class="text-center text-gray-500 text-sm mx-2">{{ user.introduction }}</p>
+              </div>
+            </div>
+            <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+              <div class="leading-relaxed text-lg mb-4">
+                <div v-if="posts[0]">
+                  <Lists :posts="posts" @getPosts="getPosts()" />
+                </div>
+                <div v-else>
+                  <p class="text-gray-500 text-md">投稿はありません。</p>
+                </div>
+              </div>
+            </div>
+            <createForm />
+          </div>
         </div>
-        <section class="text-gray-600 body-font overflow-hidden">
-          <Lists :posts="posts" @getPosts="getPosts()" />
-        </section>
       </div>
-      <div class="col-span-7 xl:col-span-2 lg:col-span-2 ml-16 mb-40">
-        <div class="xl:fixed lg:fixed xl:top-60 lg:top-60 w-11/12 lg:w-60 xl:w-60">
-          <h2 class="text-xl font-mono">{{ user.name }}</h2>
-          <p class="text-sm text-gray-400 xl:text-right lg:text-right mt-1">{{ user.created_at | moment2 }}~</p>
-          <h3 class="text-gray-500 text-lg italic my-3">{{ user.introduction }}</h3>
-        </div>
-        <createForm />
-      </div>
-    </div>
-    
+    </section>
   </div>
 </template>
 
